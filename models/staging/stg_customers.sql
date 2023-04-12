d0f610f6589fea896af56b69579864c9d0ba7777
Bethany Hipple <bethany.hipple@fishtownanalytics.com>
2023-04-12 21:06:15 +0000

    edited formatting to follow dbt style guide and deleted source.yml and source function

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,13 +1,11 @@
-with
-
-customers as (
+with customers as (
 
 select
     id as customer_id,
     first_name,
     last_name
 
-from {{ source('jaffle_shop','customers') }}
+from raw.jaffle_shop.customers
 
 )
 
